1c6937745bd4d61741ba1a6475301877ef34106d
Nguyễn Thành Long <104711708+AIMH9999@users.noreply.github.com>
2023-03-10 09:13:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,40 @@
-'BÀI 1: Chương trình kiểm tra số chẵn hay lẻ trong Python'
+'Tuổi của một người'
 
-n = float(input("Nhập vào một số:"))
-if n % 2 == 0 and n != 0:
-    print("n là số chẵn")
-elif n % 2 == 1:
-    print("n là số lẻ")
-else :
-    print("n không phải là số chẵn cũng không phải là số lẽ")
-
-'BÀI 2: Chương trình tìm số lớn nhất trong 3 số bằng Python'
-
-a = int(input("Nhập số a:"))
-b = int(input("Nhập số b:"))
-c = int(input("Nhập số c:"))
-max = a
-if b > max:
-    max = b
-if c > max:
-    max = c
-print("Max = %d" %max)
-
-'BÀI 3:	Chương trình in ra các ngày trong tháng bằng Python'
-
-month = 30
-day = 1
-while day <= month:
-    print("Ngày %d" %day)
-    day = day + 1
-
-'''
-Bài 4:Viết chương trình cho người dùng nhập vào một số nguyên n.
-Nếu n lẻ thì in ra màn hình dòng chữ “Số lẻ”. Nếu số n chẵn và lớn hơn hoặc bằng 100 thì in ra màn hình dòng chữ “Số chẵn và lớn hơn hoặc bằng 100”, 
-ngược lại thì in ra dòng chữ “Số chẵn và bé hơn 100”.
-'''
-
-n = int(input("Nhập số nguyên n:"))
-if n % 2 == 0 and n != 0:
-    if n >= 100:
-        print("n là số chẵn và n lớn hơn hoặc bằng 100")
-    else:
-        print("n là số chẵn và n bé hơn 100")
-elif n % 2 == 1:
-    if n >= 100:
-        print("n là số lẻ và n lớn hơn hoặc bằng 100")
-    else:
-        print("n là số lẽ và n bé hơn 100")
+age = int(input("Nhập tuổi của một người: "))
+if age > 0 and age <= 2:
+    print("Là trẻ sơ sinh")
+elif age > 2 and age <= 10:
+    print("Là nhi đồng")
+elif age > 10 and age <= 17:
+    print("Là vị thành niên")
+elif age > 17 and age <= 39:
+    print("Là thanh niên")
+elif age > 39 and age <= 50:
+    print("Là trung niên")
+elif age > 50:
+    print("Là cao niên")
 else:
-    print("n = 0")
+    print("Nhập sai")
 
-'Bài 5: Biện luận phương trình bậc nhất ax+b=0'
+'Năm SX của một chiếc máy tính'
 
-a = int(input("Nhập a:"))
-b = int(input("Nhap b:"))
-
-if a == 0 and b !=0:
-    print("Phương trình vô nghiệm")
-elif a != 0 and b == 0:
-    print("Phương trình có một nghiệm x = 0")
-elif a == 0 and b == 0:
-    print("Phương trình có vô số nghiệm")
+year = int(input("Nhập năm sản xuất của một chiếc máy tính: "))
+if year >= 15:
+    print("Thay thế")
+elif year >= 10 and year < 15:
+    print("Bảo trì")
 else:
-    print("Phương trình có một nghiệm = %.1f"%(-b/a))
+    print("Không có đề xuất")
 
+'Điểm trung bình va xét học bổng'
 
+GPA = float(input("Nhập điểm trung bình:"))
+if GPA >= 9:
+    print("Học bổng là 5.000.000 VNĐ")
+elif GPA >= 8 and GPA < 9:
+    print("Học bổng là 3.000.000 VNĐ")
+elif GPA >= 7 and GPA < 8:
+    print("Học bổng là 1.000.000 VNĐ")
+else:
+    print("Không có học bổng")
 
